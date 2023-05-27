81d085e1dcb8b19b18a1c2cb92ef3e48de8606f4
Violetta Korolkova <113035992+VioK0709@users.noreply.github.com>
2023-05-27 21:53:55 +0200

    Update Handler.java

--- src/main/java/org/example/Handler.java
+++ src/main/java/org/example/Handler.java
@@ -2,7 +2,7 @@ package org.example;
 
 import java.io.BufferedOutputStream;
 
-@FunctionalInterface    //функциональный интерфейс только с 1 методеом обработки запроса и выддачи? выходного потока
+@FunctionalInterface  
 public interface Handler {
     void handler(Request request, BufferedOutputStream outputStream);
 }
